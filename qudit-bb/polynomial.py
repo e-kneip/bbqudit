cb268e14891c3d790c9c2ca2f46ed4acdfa9fccc
Eleanor Kneip <eleanor.kneip2705@gmail.com>
2025-02-25 17:45:05 +0000

    Feat: Polynomial upgraded to bivariate

--- qudit-bb/polynomial.py
+++ qudit-bb/polynomial.py
@@ -1,41 +1,26 @@
 """Implementation of the Polynomial class over finite fields."""
 
+import numpy as np
+
 class Polynomial:
     """Polynomial class over finite fields."""
 
     def __init__(self, field, coefficients):
         if type(field) is not int:
             raise TypeError("field must be an integer")
-        if type(coefficients) is not list:
-            raise TypeError("coefficients must be a list")
-        for i in coefficients:
-            if type(i) is not int:
-                raise TypeError("Coefficients must be a list of integers")
+        if type(coefficients) is not np.ndarray:
+            raise TypeError("coefficients must be a ndarray")
+        if coefficients.dtype != int:
+            raise TypeError("coefficients must be an ndarray of integers")
+        if coefficients.ndim != 2:
+            raise ValueError("coefficients must be a 2D array")
         self.field = field
-        self.coefficients = [i % field for i in coefficients]
+        self.coefficients = coefficients % field
 
     def __str__(self):
         """String representation of Polynomial."""
-        return " + ".join([f"{self.coefficients[i]}x^{i}" for i in range(len(self.coefficients))])
+        return " + ".join([f"{self.coefficients[i, j]}x^{i}y^{j}" for i in range(self.coefficients.shape[0]) for j in range(self.coefficients.shape[1])])
 
     def __repr__(self):
         """Canonical string representation of Polynomial."""
         return f"Polynomial({self.field}, {self.coefficients})"
-
-    def __add__(self, other):
-        """Add two Polynomials."""
-        if self.field != other.field:
-            raise ValueError("Fields must be the same")
-        common = min(len(self.coefficients), len(other.coefficients))
-        result = [self.coefficients[i] + other.coefficients[i] for i in range(common)]
-        result += self.coefficients[common:] + other.coefficients[common:]
-        return Polynomial(self.field, result)
-
-    def __sub__(self, other):
-        """Subtract two Polynomials."""
-        if self.field != other.field:
-            raise ValueError("Fields must be the same")
-        common = min(len(self.coefficients), len(other.coefficients))
-        result = [self.coefficients[i] - other.coefficients[i] for i in range(common)]
-        result += self.coefficients[common:] + [-i for i in other.coefficients[common:]]
-        return Polynomial(self.field, result)
