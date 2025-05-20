cbaf1459392a67067c16e61db02c151e25e37436
Eleanor Kneip <eleanor.kneip2705@gmail.com>
2025-05-20 11:15:37 +0100

    Feat: setup decoder

---
+++ bbq/decoder.py
@@ -0,0 +1,11 @@
+"""Implementation of a selection of decoders for qudits."""
+
+# Dijkstra decoder
+
+# Belief propagation decoder
+
+# Ordered statistics decoder
+
+# D+OSD decoder
+
+# BP+OSD decoder
